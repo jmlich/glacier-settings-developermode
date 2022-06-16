c19fea25c1f0b8ff8e6c287dd47c39604f8364a9
Chupligin Sergey <neochapay@gmail.com>
2022-06-16 10:54:09 +0000

    Add plugin

---
+++ plugin/developermode.cpp
@@ -0,0 +1,88 @@
+/*
+ * Copyright (C) 2022 Chupligin Sergey <neochapay@gmail.com>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Library General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU Library General Public License
+ * along with this library; see the file COPYING.LIB.  If not, write to
+ * the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+ * Boston, MA 02110-1301, USA.
+ */
+
+#include "developermode.h"
+
+#include <QDBusInterface>
+#include <QFileSystemWatcher>
+
+DeveloperMode::DeveloperMode(QQuickItem *parent)
+    : QObject{parent}
+    , m_available(QFile::exists("/usr/bin/sshd"))
+    , m_enabled(false)
+{
+    QDBusConnection systemDBusConnection = QDBusConnection::systemBus();
+
+    QFileSystemWatcher sshdWatcher;
+    sshdWatcher.addPath("/usr/bin/sshd");
+    connect(&sshdWatcher, &QFileSystemWatcher::fileChanged, this, &DeveloperMode::onSshDChanged);
+
+
+    QDBusInterface dbInterface(
+                "org.nemomobile.developermode"
+                , "/"
+                , "org.nemomobile.developermode"
+                , systemDBusConnection);
+
+    if (dbInterface.isValid () ) {
+        if(dbInterface.property("enabled").toBool()) {
+            m_enabled = true;
+        }
+    } else {
+        qWarning("Developermode systemd interface not found");
+    }
+
+    QDBusConnection::systemBus().connect(
+                QString(),
+                "org.nemomobile.developermode",
+                "org.freedesktop.DBus.Properties",
+                "PropertiesChanged",
+                this,
+                SLOT(propertiesChanged(QString, QVariantMap, QStringList)));
+}
+
+void DeveloperMode::setEnabled(bool enable)
+{
+    if(!m_available) {
+        return;
+    }
+
+    if(m_enabled != enable) {
+        m_enabled = enable;
+        emit enabledChanged();
+    }
+}
+
+void DeveloperMode::onSshDChanged(QString path)
+{
+    bool ssdAvaileble = QFile::exists("/usr/bin/sshd");
+    if(ssdAvaileble != m_available) {
+        m_available = ssdAvaileble;
+        emit availableChanged();
+    }
+}
+
+void DeveloperMode::propertiesChanged(const QString &, const QVariantMap &properties, const QStringList &)
+{
+    bool enabled = properties.value("enabled", QString()).toBool();
+    if(enabled != m_enabled) {
+        m_enabled = enabled;
+        enabledChanged();
+    }
+}
---
+++ plugin/developermode.h
@@ -0,0 +1,54 @@
+/*
+ * Copyright (C) 2022 Chupligin Sergey <neochapay@gmail.com>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Library General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU Library General Public License
+ * along with this library; see the file COPYING.LIB.  If not, write to
+ * the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+ * Boston, MA 02110-1301, USA.
+ */
+
+#ifndef DEVELOPERMODE_H
+#define DEVELOPERMODE_H
+
+#include <QDBusConnection>
+#include <QObject>
+#include <QQuickItem>
+
+class DeveloperMode : public QObject
+{
+    Q_OBJECT
+    Q_PROPERTY(bool available READ available NOTIFY availableChanged)
+    Q_PROPERTY(bool enabled READ enabled WRITE setEnabled NOTIFY enabledChanged)
+
+public:
+    explicit DeveloperMode(QQuickItem *parent = nullptr);
+
+    bool available() {return m_available;}
+    bool enabled() {return m_enabled;}
+
+    void setEnabled(bool enable);
+
+signals:
+    void availableChanged();
+    void enabledChanged();
+
+private slots:
+    void onSshDChanged(QString path);
+    void propertiesChanged(const QString &, const QVariantMap &properties, const QStringList &);
+
+private:
+    bool m_available;
+    bool m_enabled;
+};
+
+#endif // DEVELOPERMODE_H
